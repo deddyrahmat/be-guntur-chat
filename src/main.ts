222cf76bc5700e9b2c7d52ffebb174af2a7423aa
deddy rahmat <deddyrahmat97@gmail.com>
2024-09-18 12:08:51 +0700

    fix: cors
    update url for cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'], // Beberapa origin yang diizinkan
+    origin: ['https://fe-guntur-chat.vercel.app/login'], // Beberapa origin yang diizinkan
   });
   await app.listen(3000);
 }
