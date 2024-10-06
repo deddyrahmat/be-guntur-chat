a762d8ae245cedfabe6ec74c2cf7ef2148aa653d
deddy rahmat <deddyrahmat97@gmail.com>
2024-10-06 21:28:28 +0700

    feat: enable cors for all

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://fe-guntur-chat.vercel.app'], // Beberapa origin yang diizinkan
+    origin: ['*'], // Beberapa origin yang diizinkan
   });
   await app.listen(3000);
 }
